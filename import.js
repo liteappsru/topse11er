bbc1d409e1fd0b69e516fe4fc285717b980e97e2
nkrylov <nkrylovbit@gmail.com>
2019-07-30 19:40:32 +0300

    Правки в дизайне

--- import.js
+++ import.js
@@ -5,79 +5,61 @@ var assert = require('assert');
 var dateFormat = require('dateformat');
 
 const config = {
-    since:'2019-07-01T00:00:00.032Z',
-    to:'2019-08-01T00:00:00.033Z',
-    ozonID:'698',
-    ozonAPIKey:'7a562dfd-61cf-4fd3-a629-735924f53e5f',
-    wbUserName:'sup-26735-20092',
-    wbPassword:'vj5I69U6',
-    wbReport_id:'784485'
+    ozon:{
+        since:'2019-07-01T00:00:00.032Z',
+        to:'2019-08-01T00:00:00.033Z',
+        method: "POST",
+        uri: "https://api-seller.ozon.ru/v1/order/list",
+        uriOrder:"https://api-seller.ozon.ru/v1/order/",
+        headers: {
+            'Content-Type': 'application/json',
+            //'Content-Length': ozonBody.length,
+            'Client-Id':'698',
+            'Api-Key': '7a562dfd-61cf-4fd3-a629-735924f53e5f'
+        }
+    },
+    wb:{
+        wbUserName:'sup-26735-20092',
+        wbPassword:'vj5I69U6',
+        wbReportIds:['794566',
+            '784485',
+            '781702',
+            '770060'],
+        method: "POST",
+        uriSite:"https://suppliers.wildberries.ru",
+        uriAuth: "https://suppliers.wildberries.ru/Account/Login",
+        uriReport: "https://suppliers.wildberries.ru/realization/getreportdetails/",
+        headerAuth: {
+            'Content-Type': 'application/json'
+        }
+    }
 }
 
-const ozonBody = {
-    since:config.since,
-    to:config.to,
-    delivery_schema:'fbo',
-    page:1,
-    page_size:1000
-};
-
-const ozonConfig = {
-    method: "POST",
-    uri: "https://api-seller.ozon.ru/v1/order/list",
-    uriOrder:"https://api-seller.ozon.ru/v1/order/",
-    headers: {
-        'Content-Type': 'application/json',
-        //'Content-Length': ozonBody.length,
-        'Client-Id':config.ozonID,
-        'Api-Key':config.ozonAPIKey
-    }
-};
-
 var ozonOptions = {
-    uri: ozonConfig.uri,
-    method: ozonConfig.method,
-    body: ozonBody,
+    uri: config.ozon.uri,
+    method: config.ozon.method,
+    body: {
+        since:config.ozon.since,
+        to:config.ozon.to,
+        delivery_schema:'fbo',
+        page:1,
+        page_size:1000
+    },
     json: true,
-    headers: ozonConfig.headers
+    headers: config.ozon.headers
 };
 
 const wbAuthBody = {
-    'UserName':config.wbUserName,
-    'Password':config.wbPassword
-};
-
-const wbOrderBody = {
-    "length": -1,
-    "reportId":"784485",
-    "draw":2,
-    "start":0,
-    "order":
-        {
-            "column":0,
-            "dir":"desc"},
-    "search":{
-        "value":"",
-        "regexp":false}
-}
-
-const wbConfig = {
-    method: "POST",
-    uriSite:"https://suppliers.wildberries.ru",
-    uriAuth: "https://suppliers.wildberries.ru/Account/Login",
-    uriReport: "https://suppliers.wildberries.ru/realization/getreportdetails/",
-    headerAuth: {
-         'Content-Type': 'application/json'
-    }
+    'UserName':config.wb.wbUserName,
+    'Password':config.wb.wbPassword
 };
 
 const wbAuthOptions = {
-    url: wbConfig.uriAuth,
-    method: wbConfig.method,
+    url: config.wb.uriAuth,
+    method: config.wb.method,
     body: wbAuthBody,
     json: true,
-    //followAllRedirects: true,
-    headers: wbConfig.headerAuth,
+    headers: config.wb.headerAuth,
     followRedirect: true
 };
 
@@ -89,7 +71,7 @@ function ozonAuthCallback(error, response, body) {
     for (var i = 0; i<body.result.order_ids.length; i++) {
         var orderid = body.result.order_ids[i];
         ozonOptions.method = "GET",
-        ozonOptions.uri = ozonConfig.uriOrder + orderid;
+        ozonOptions.uri = config.ozon.uriOrder + orderid;
         ozonOptions.body = "";
         request(ozonOptions, ozonOrderCallback);
     }
@@ -115,7 +97,7 @@ function ozonOrderCallback(error, response, body) {
                     "item_id": item.item_id,
                     "product_id": item.product_id,
                     "quantity": item.quantity,
-                    "price": Number.parseInt(item.price)}
+                    "price": Number.parseFloat(item.price)}
                 },
                 {"upsert": true}
             );
@@ -129,48 +111,71 @@ function wbAuthCallback(error, response, body) {
 
     const wbCookies = response.headers['set-cookie'];
 
-    const wbOrdersOptions = {
-        'url': wbConfig.uriReport,
-        'method': wbConfig.method,
-        'body': wbOrderBody,
-        'json': true,
-        'headers': {cookie:wbCookies[0]+','+wbCookies[1]+','+wbCookies[2]}
-    };
-    request(wbOrdersOptions, wbOrderCallback);
+    for (let i=0; i<config.wb.wbReportIds.length;i++){
+        let wbOrderBody = {
+            "length": -1,
+            "reportId":config.wb.wbReportIds[i],
+            "draw":2,
+            "start":0,
+            "order":
+                {
+                    "column":0,
+                    "dir":"desc"},
+            "search":{
+                "value":"",
+                "regexp":false}
+        }
+
+        let wbOrdersOptions = {
+            'url': config.wb.uriReport,
+            'method': config.wb.method,
+            'body': wbOrderBody,
+            'json': true,
+            'headers': {cookie:wbCookies[0]+','+wbCookies[1]+','+wbCookies[2],orderid:config.wb.wbReportIds[i]}
+        };
+        console.log('Получение заказов wb.' + "reportId:" + config.wb.wbReportIds[i])
+        request(wbOrdersOptions, wbOrderCallback);
+    }
 };
 
 function wbOrderCallback(error, response, body) {
     //console.log('body:', body);
     if (!(response==undefined) && response.statusCode==200) {
+        let orderid = response.request.headers.orderid;
         MongoClient.connect(url, function (err, client) {
             var db = client.db('topse11er');
+            console.log('Получены данные.' + "reportId:" + config.wb.wbReportIds[i])
+            console.log('Всего строк:' + body.data.length);
             for (var i = 0; i < body.data.length; i++) {
                 var item = body.data[i];
-                var order_time = new Date(item.SaleDate);
-                order_time.setHours(0);
-                order_time.setMinutes(0);
-                order_time.setSeconds(0);
-                order_time.setMilliseconds(0);
 
-                var item_id = item.ShkId+"."+i;
-                try{
-                    db.collection('sales').updateOne(
-                        {"item_id": item_id},
-                        {
-                            $set: {
-                                "shop": "wb",
-                                "date": order_time,
-                                "item_id": item_id,
-                                "product_id": item.SupplierArticle,
-                                "quantity": item.Quantity,
-                                "price": Number.parseFloat(item.RetailAmount)
+                if (!(item.Quantity==0)) {
+                    var order_time = new Date(item.SaleDate);
+                    order_time.setHours(0);
+                    order_time.setMinutes(0);
+                    order_time.setSeconds(0);
+                    order_time.setMilliseconds(0);
+
+                    let item_id = orderid + '.' + item.ShkId + '.' + i;
+                    try{
+                        db.collection('sales').updateOne(
+                            {"item_id": item_id},
+                            {
+                                $set: {
+                                    "shop": "wb",
+                                    "date": order_time,
+                                    "item_id": item_id,
+                                    "product_id": item.SupplierArticle,
+                                    "quantity": item.Quantity,
+                                    "price": Number.parseFloat(item.RetailAmount)
+                                }
                             }
-                        }
-                        , {upsert: true}
-                    )
-                }
-                catch (e) {
-                    console.log(e);
+                            , {upsert: true}
+                        )
+                    }
+                    catch (e) {
+                        console.log(e);
+                    }
                 }
             }
             client.close();
@@ -232,9 +237,9 @@ function saveSalesByDay(docs){
 }
 
 function collect(){
-    request(ozonOptions, ozonAuthCallback);
-    request(wbAuthOptions, wbAuthCallback);
+    //request(ozonOptions, ozonAuthCallback);
+    //request(wbAuthOptions, wbAuthCallback);
     aggregate();
 }
 
-collect();
+//collect();
