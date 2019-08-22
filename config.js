d061be5c97eb846b110e0f7bf14ba22e6bb13ae3
nkrylov <nkrylovbit@gmail.com>
2019-08-23 00:10:19 +0300

    +Уточнил дату начала закачки

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     dbName: 'topse11er',
     url: 'mongodb://localhost:27017/topse11er',
     ozon:{
-        since:'2019-07-01T00:00:00.032Z',
+        since:'2019-08-01T00:00:00.032Z',
         to:'2020-08-01T00:00:00.033Z',
         method: "POST",
         uri: "https://api-seller.ozon.ru/v1/order/list",
